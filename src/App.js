2ebc72b32900b1180208feb384f28328aa6d0652
Daniela Romero <fadlala.daniela@gmail.com>
2022-04-12 19:41:18 -0300

    entrega

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Cart from "./Componentes/Cart/Cart";
 import NavBar from "./Componentes/NavBar/NavBar";
 import ItemListContiner from "./Contenedores/ItemListContiner";
 import ItemDetailContainer from "./Componentes/ItemDetailContainer/ItemDetailContainer";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //MIS NOTAS: rfce o rce export component o rafce arrow funtion
@@ -15,11 +14,9 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<ItemListContiner saludo="Bienvenidos!" />} />
-        <Route path="/detail /: id" element={<ItemDetailContainer />} />
+        <Route path="/detail/:id" element={<ItemDetailContainer />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
-      <ItemListContiner saludo="Bienvenidos a la pagina" />
-      <ItemDetailContainer />
     </BrowserRouter>
   );
 }
